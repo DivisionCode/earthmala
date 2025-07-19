c76543fcca924dece224b273a33243c76b9e8a15
divisioncode <singh.rsingh.rohit@gmail.com>
2025-07-20 00:44:57 +0530

    correction and updation done for deployement

--- client/src/pages/Products.vue
+++ client/src/pages/Products.vue
@@ -28,7 +28,7 @@ const selectedArt = ref(null);
 
 onMounted(async () => {
   try {
-    const res = await fetch('http://localhost:5000/api/artworks');
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/api/artworks`);
     const data = await res.json();
 
     artworks.value = data.map(item => ({
